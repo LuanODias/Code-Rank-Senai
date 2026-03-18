a614213074a6c6de7440f364b243eedf54444e1c
LuanODias <luoliveidias@gmail.com>
2026-03-17 21:08:24 -0300

    feat: add admin controller

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,34 @@
+class AdminController {
+  constructor(adminService) {
+    this.adminService = adminService;
+  }
+
+  async create(req, res, next) {
+    try {
+      const admin = await this.adminService.create(req.body);
+      res.status(201).json(admin);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async remove(req, res, next) {
+    try {
+      await this.adminService.remove(req.params.userId);
+      res.status(204).send();
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  generateToken(req, res, next) {
+    try {
+      const result = this.adminService.generateToken(req.body.label);
+      res.json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = { AdminController };
