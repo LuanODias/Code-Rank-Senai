06183a4fd9705730a7145537a059b2ec47632adf
LuanODias <luoliveidias@gmail.com>
2026-03-17 21:50:26 -0300

    feat: inject auth repository into auth factory

--- src/factories/auth.factory.js
+++ src/factories/auth.factory.js
@@ -1,10 +1,14 @@
+const { getPrismaClient } = require('../config/prisma');
 const { getAuth } = require('../config/auth');
+const { AuthRepository } = require('../repositories/auth.repository');
 const { AuthService } = require('../services/auth.service');
 const { AuthController } = require('../controllers/auth.controller');
 
 const makeAuthController = () => {
+  const prisma = getPrismaClient();
   const auth = getAuth();
-  const service = new AuthService(auth);
+  const repository = new AuthRepository(prisma);
+  const service = new AuthService(auth, repository);
   return new AuthController(service);
 };
 
