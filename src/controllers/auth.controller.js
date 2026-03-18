deb3b114bbfaef5c2e8a50de57693e0bf8e1cc62
LuanODias <luoliveidias@gmail.com>
2026-03-17 21:15:20 -0300

    feat: add auth controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+class AuthController {
+  constructor(authService) {
+    this.authService = authService;
+  }
+
+  async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const result = await this.authService.login(email, password);
+      res.json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+      await this.authService.logout(req.headers);
+      res.status(204).send();
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = { AuthController };
